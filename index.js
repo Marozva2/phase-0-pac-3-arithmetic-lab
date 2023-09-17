ffd45f4ea17ce5ada1d346958ee2ed9b77d2f8b3
Marozva <isabokefrankline@gmail.com>
2023-09-17 15:35:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+//add() function
+function add(a, b) {
+    return a + b;
+}
+
+//subtract() function
+function subtract(a, b) {
+    return a - b;
+}
+
+//multiply() function
+function multiply(a, b) {
+    return a * b;
+}
+
+//divide() function
+function divide(a, b) {
+    return a / b;
+}
+
+//increment() function
+function increment(n) {
+    return ( n + 1 );
+}
+
+//decrement() function
+function decrement(n) {
+    return ( n - 1 );
+}
+
+//parse() function
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+//preserve() function
+function preserveDecimal (n) {
+    return parseFloat(n, 10);
+}
